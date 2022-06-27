7d53f4150fa6b1be24574962b50349a7be9c5f61
Jacob Benowitz <jacob.benowitz@gmail.com>
2022-06-27 17:30:33 -0400

    update package and webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,11 @@
 // webpack.config.js
-var path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
 
 module.exports = {
-    
-    entry: [
-        path.resolve(__dirname, 'frontend', 'widgets.jsx'),
-        path.resolve(__dirname, 'index.css'),
-    ],
+    context: __dirname,
+    entry: './frontend/widgets.jsx',
     output: {
-        path: path.join(__dirname, 'dist'), // bundled file in dist/
+        path: path.join(__dirname),
         filename: 'bundle.js',
     },
     module: {
@@ -19,64 +15,15 @@ module.exports = {
                 exclude: /(node_modules)/,
                 use: {
                     loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    query: {
+                        presets: ['@babel/env', '@babel/react']
                     }
-                }
-            },
-            {
-                test: /\.s?[ac]ss$/, 
-                use: [
-                    MiniCssExtractPlugin.loader, // create bundled css file
-                    {
-                        loader: 'css-loader', 
-                        options: { url: false } 
-                    },
-                ]
+                },
             }
-        ],
+        ]
     },
-    plugins: [
-        new MiniCssExtractPlugin()
-    ],
     devtool: 'source-map',
     resolve: {
-        extensions: ['.js', '.jsx', '*']
+        extensions: [".js", ".jsx", "*"]
     }
 };
-
-// module.exports = {
-    
-//     entry: [
-//         path.resolve(__dirname, 'frontend', 'widgets.jsx'),
-//         path.resolve(__dirname, 'index.css'),
-//     ],
-//     output: {
-//         path: path.join(__dirname, 'dist'), // bundled file in dist/
-//         filename: 'bundle.js',
-//     },
-//     module: {
-//         rules: [
-//             {
-//                 test: /\.js$/, // applies to js files
-//                 use: ['babel-loader'], // transpiles your js
-//                 exclude: /node_modules/, // don't transpile node modules
-//             },
-//             {
-//                 test: /\.s?[ac]ss$/, // applies to css/scss/sass files
-//                 use: [
-//                     MiniCssExtractPlugin.loader, // create bundled css file
-//                     {
-//                         loader: 'css-loader', // resolves @import statements
-//                         options: { url: false } // don't resolve url() statements
-//                     },
-//                 ]
-//             }
-//         ],
-//     },
-//     plugins: [new MiniCssExtractPlugin()],
-//     devtool: 'source-map',
-//     resolve: {
-//         extensions: ['.js', '.jsx', '*']
-//     }
-// };
